850077219d6f0dfd9f4c36c96cb29b48df3c3ea7
dazhi1204 <17600451741@163.com>
2020-05-19 14:12:20 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import Vuex from 'vuex'
+
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 import 'normalize.css/normalize.css'
+import  VueResource  from 'vue-resource'
 
 Vue.use(Vuex)
+Vue.use(VueResource)
 Vue.use(ElementUI, {
   size: 'mini'
 })
